fd466ac711a23455b61b0e68655ab7d8fe99ce2f
Yuuto Harada <b1018185.fun@gmail.com>
2020-05-30 17:41:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/rdnotepad/TopActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rdnotepad
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top)
+
+    }
+}
